bf31cc526c403098c22637055e1bd5bc5a84463a
sounakchatterjee00 <sounakchatterjee00@gmail.com>
2020-06-23 20:50:06 +0530

    add events

--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v2.0! Your function executed successfully!',
+        message: 'Go Serverless v3.0! Your function executed successfully!',
         input: event,
         timestamp : moment().unix()
       },
