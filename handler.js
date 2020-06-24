03fe95629c131cd8a5af5b144291671acae6d9f2
sounakchatterjee00 <sounakchatterjee00@gmail.com>
2020-06-24 14:14:26 +0530

    ne line added1.0

--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v6.0! Your function executed successfully!new line added',
+        message: 'Go Serverless v6.0! Your function executed successfully!new line added1.0',
         input: event,
         timestamp : moment().unix()
       },
