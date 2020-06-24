f37f86932ec955a05efe129816bb86187d6ea099
sounakchatterjee00 <sounakchatterjee00@gmail.com>
2020-06-24 10:22:48 +0530

    testing change

--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v5.0! Your function executed successfully!',
+        message: 'Go Serverless v6.0! Your function executed successfully!',
         input: event,
         timestamp : moment().unix()
       },
@@ -15,6 +15,4 @@ module.exports.hello = async event => {
     ),
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
