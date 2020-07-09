bb5272598841a6f9c5bbe169029fd08cba440832
Sounak Chatterjee <66839304+sounakchatterjee00@users.noreply.github.com>
2020-07-09 20:40:38 +0530

    nit

--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v7.0! Your function executed successfully!new line added 2.0',
+        message: 'Go Serverless v7.0! Your function executed successfully!new line added 7.0',
         input: event,
         timestamp : moment().unix()
       },
